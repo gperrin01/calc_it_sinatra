65b3afa5696b7245d59bcc5ac73ba6004bd1ddfb
Guillaume Perrin <gperrin01@gmail.com>
2015-05-26 17:54:51 +0100

    added page basic and power

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+
+get '/' do 
+  @first = params["first"].to_f
+  @operator = params["operator"]
+  @second = params["second"].to_f
+  if @first && @second && @operator
+    @result = case @operator
+    when '+' then @first + @second
+    when '-' then @first - @second
+    when '*' then @first * @second
+    when '/' then @first / @second
+    end
+  end
+  erb :home
+end
+
+get '/power' do 
+  if @first && @power
+    @first = params["first"].to_f
+    @power = params["power"].to_i
+    @result = @first ** @power 
+  end
+end
+
+get '/sqrt' do 
+  if @first
+    @first = params["first"].to_f
+    @result = Math.sqrt(@first)
+  end
